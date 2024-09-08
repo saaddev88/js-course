0e23f8198626bd742503d7b9b9a47491ca18b44c
saaddev88 <m.saad17102002@gmail.com>
2024-09-08 16:19:51 +0000

    strings file

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name="saad"
+const NumberCount =50
+// console.log(name + NumberCount +"resullog
+console.log(`hey this is ${name}and my number is${NumberCount}`);
+
+const gameName = new String( "  saadpc" )
+
+console.log(gameName [3]);
+console.log(gameName.length);
+console.log(gameName.bold());
+console.log(gameName.toUpperCase());
+console.log(gameName.trim());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('d'));
+const newstrings =gameName.substring(0,5)
+console.log(newstrings);
+
+const anotherstring = gameName.slice(2,6)
+console.log(anotherstring);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
