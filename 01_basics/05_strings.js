1bf6b23e654c71d5f6d423b00572f2281289753f
saaddev88 <m.saad17102002@gmail.com>
2024-09-08 20:46:00 +0000

    strings file

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,9 +1,11 @@
+// ***********************************strings and its function*******************************************
+
 const name="saad"
 const NumberCount =50
 // console.log(name + NumberCount +"resullog
 console.log(`hey this is ${name}and my number is${NumberCount}`);
 
-const gameName = new String( "  saadpc" )
+const gameName = new String( "saadpc-se-engineer" )
 
 console.log(gameName [3]);
 console.log(gameName.length);
@@ -14,9 +16,22 @@ console.log(gameName.charAt(5));
 console.log(gameName.indexOf('d'));
 const newstrings =gameName.substring(0,5)
 console.log(newstrings);
+const anotherString = gameName.slice(-6,-2)
+console.log(anotherString);
 
-const anotherstring = gameName.slice(2,6)
-console.log(anotherstring);
+const url = "https\= m.saad@.com"
+console.log(url.replace('@' ,'17102002'));
+
+console.log(url.includes("saad"));
+console.log(url.includes("aslam"));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
 
 
 
