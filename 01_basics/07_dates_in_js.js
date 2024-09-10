963afbf83e06000b4f93a50d5f45d4d5acf6a4bb
saaddev88 <m.saad17102002@gmail.com>
2024-09-10 23:44:38 +0000

    date and time file

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,45 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024,0,7,5,33,30)
+// let myCreatedDate = new Date("2023-04-02")
+let myCreatedDate = new Date("01-15-2023")
+
+
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toLocaleString());
+let myTimesmap = Date.now()
+// console.log(myTimesmap)
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getTime());
+console.log(newDate.getMonth() +1);
+
+newDate.toLocaleString('default', {weekday:"long"}
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
