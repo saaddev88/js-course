33b8d7a7e1cff8ff625674d2024cb73f40c59c2b
saaddev88 <m.saad17102002@gmail.com>
2024-09-10 22:30:45 +0000

    num and maths file

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,40 @@
+// **********************************************Numbers************************************************************
+const score = 300
+// console.log(score);
+
+const stringnum = new Number(400.674848);
+// console.log(stringnum);
+// console.log(stringnum.toFixed(2));
+// console.log(stringnum.toPrecision(3));
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-Pk'));
+// *********************************************************MatheMatics***********************************************************
+console.log(Math);
+console.log(Math.abs(-7));
+console.log(Math.round(7.6));
+console.log(Math.ceil(5.7));
+console.log(Math.floor(7.8));
+console.log(Math.min(4,6,8,56,33));
+console.log(Math.max(5,8,78,65,33));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max - min +1)) +min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
