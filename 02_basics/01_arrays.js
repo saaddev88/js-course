1d23cc6b6f9ece5630b726f7bbec4a5056a53452
saaddev88 <m.saad17102002@gmail.com>
2024-09-12 22:16:25 +0000

    arrays_file

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+// ARRAYS
+const myArr = [1,4,6,7,8]
+// console.log(myArr);
+
+const newArr = new Array(1,2,3,4)
+// console.log(newArr[3]);
+
+// ARRAY METHODS
+
+// myArr.push(8)
+// myArr.push(10)
+// myArr.pop()
+// myArr.unshift(7)
+// myArr.shift()
+// console.log(myArr.includes(7));
+// console.log(myArr.indexOf(8));
+
+// const anotherArray = newArr.join()
+
+// console.log(myArr);
+// console.log(anotherArray);
+// console.log(typeof anotherArray);
+
+// slice,splice 
+console.log("A", myArr);
+console.log("B", myArr);
+ const myn1 = myArr.slice(1,3)
+ console.log(myn1);
+
+ 
+ const myn2 = myArr.splice(1,3)
+ console.log("C",myArr);
+ console.log(myn2);
+
+ 
+
+ 
+ 
+ 
+
+
+
+
+
