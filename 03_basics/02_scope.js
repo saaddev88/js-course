d830ff903e42279aeb74a7e80ffd50849fa69f65
saaddev88 <m.saad17102002@gmail.com>
2024-09-21 20:52:57 +0000

    scope file

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,37 @@
+// ************************scope**************************
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// if these {} is used in function,if/else condition it is called scope.
+
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     var c = 30
+
+
+
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);// this is the problem we dont use var in any scope because it is print outside of the scope and in scope it is not good because in scope only that thing is out of scope which in return form and var not follow this thats why we dont use var. and which is written in {} it is called block scope and outside of it {} is called global scope
+// let a =200
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     var c = 30
+
+//     console.log("inner:",a);
+    
+
+
+// }
+// console.log(a);
+
+// in the above example the global scope not entered in the block scope and which written in block scope not come outside
+
