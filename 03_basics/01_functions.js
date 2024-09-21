75ba6409f31a4309754a20da82911a8f5f2318c9
saaddev88 <m.saad17102002@gmail.com>
2024-09-21 20:24:33 +0000

    function file

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -45,8 +45,58 @@ function userloggedin(username = "sam")  {
         
 
     }
-    return `${username} "just logged In"`
+    // return `${username} "just logged In"`
     
 }
 console.log(userloggedin())
 
+// In shopping cart user adds the product countlessly in this situation we use this
+// function calculateCartPrice(num1) {
+//     return num1
+    
+// }
+// console.log(calculateCartPrice(300));
+
+// function calculateCartPrice(...num1) {
+//     return num1
+    
+// }
+// console.log(calculateCartPrice(300,200,400,600));//in this we only find first value and for complete value we use rest operator which is denoted by dots(...) and get the complete answer in array 
+
+// function calculateCartPrice(val1,val2,...num1) { //in this case if the user give the parameters like this only 2 values are console
+//     return num1
+    
+// }
+// console.log(calculateCartPrice(300,200,400,600))
+
+//in below we use object and then excess in function
+// const user = {
+
+//     name  : "saad",
+//     id : 1223
+
+
+// }
+
+// function objecthandling(anyobject) {
+//     console.log(`my name is ${anyobject.name} and my id is ${anyobject.id}`);
+    
+
+    
+// }
+// // objecthandling(user)
+
+// objecthandling({
+//     name :"saad", 
+//     id:1234,
+// })
+
+const myArray = [2,4,5,7,8,] //this method is used for access array by using functions
+ function returnthirdvalue(getArray) {
+    return getArray[2]
+    
+ }
+//  console.log(returnthirdvalue(myArray))OR
+console.log(returnthirdvalue([2,4,5,7,8,]));
+
+ 
