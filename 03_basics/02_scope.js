d62623944a992cd8a124eb6ca3a0fb01182cd17a
saaddev88 <m.saad17102002@gmail.com>
2024-09-21 21:42:07 +0000

    scope file

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -35,3 +35,55 @@
 
 // in the above example the global scope not entered in the block scope and which written in block scope not come outside
 
+function one() {
+    username ="saad"
+    function two() {
+        websitename : "youtube"
+        console.log(username);
+
+        
+    }
+    // console.log(websitename);
+    two()
+    
+    
+}
+one()
+console.log(one());
+
+if (true) {
+    const username = "saad"
+    if (username === "saad") {
+        const website = " youtube"
+        console.log(username + website);
+        
+        
+    }
+    // console.log(website);
+    
+    
+}
+
+// console.log( "username");
+// *********************interesting****************************
+
+// there are two type of function decleration
+// in simple form 
+// second form in which function is written in the variables
+
+
+function  addone(num1){
+    return num1 +1
+    
+}
+
+console.log(addone(5));
+console.log(addtwo(8));// in this addtwo is not print because it is written before initailization
+const addtwo = function addtwo(num2){
+    return num2 +2
+    
+}
+
+
+
+
