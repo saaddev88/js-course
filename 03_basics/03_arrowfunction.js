7f60ad55d39cb53d8c0f9389f3293d2dd5833eeb
saaddev88 <m.saad17102002@gmail.com>
2024-09-22 17:45:33 +0000

    arrowfunction

--- 03_basics/03_arrowfunction.js
+++ 03_basics/03_arrowfunction.js
@@ -13,21 +13,21 @@ const user = {
     }
     
 }
-// user.welcomemessage()
-// user.username = "aslam"// in this way we are able to change the value because we cant hardcore the value 
-// user.welcomemessage()
+user.welcomemessage()
+user.username = "aslam"// in this way we are able to change the value because we cant hardcore the value 
+user.welcomemessage()
 // console.log(this);// this print show the empty object because it is out of the context or outside in older day when javascript is executed in the browser (this) show the windo
 
-// function chai() {
-//     let username = "saad"
+function chai() {
+    let username = "saad"
 //     console.log(this);//This key word only used in the object thatswhy in the function printout its showa windows function
     
     
 // }
 // chai()
 
-// const chai = function () {
-//     let username = "saad"
+const chai = function () {
+    let username = "saad"
 //     console.log(this);//This key word only used in the object thatswhy in the function printout its showa windows function
     
     
@@ -35,11 +35,31 @@ const user = {
 // chai()
 //in both style of function the answer is same 
 
-
+// syntax of arrow function is ()=>{} or ()=>
 const chai = ()=> {
     let username = "saad"
     console.log(this.username);//also in arrow function is not work reason same 
     
     
 }
-chai()
+// chai()
+// if the braces is used in arrow function then it necessary to used return in the function if we want to used the value outside of scope and it known explicit return 
+const anotheruser = (num1,num2) => {
+    return num1 + num2
+// }
+
+// if the braces is not used in arrow function then it not necessary to used return in the function if we want to used the value outside of scope and it known implicit return in these case the return is by default 
+
+const anotheruser = (num1,num2) => num1 + num2
+
+// it is also return 
+const anotheruser = (num1,num2) => (num1 + num2)
+
+// if we want to used object in arrowfunction then this technique is used
+const anotheruser = (num1,num2) => ({username : "saad."})
+
+ console.log(anotheruser(2,4));
+
+
+
+
