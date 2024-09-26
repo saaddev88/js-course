3672ea5c326db8a8a81322198f25844c9cf63464
saaddev88 <m.saad17102002@gmail.com>
2024-09-26 09:26:00 +0000

    one file

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,39 @@
+// ********************************CONTROL_FLOW**************************************
+
+// this is if condition which check the condition  and choose which code is run
+
+// if (true) {
+    
+// }
+// comparison operator are: ==,====,>,<,>=,<=,!=
+
+// const temp = 50;
+// if (temp < 55) {
+//     console.log("the temperature is greater than 40");
+    
+    
+// }
+// const temp = 50;
+// if (temp === 50) {
+//     console.log("the temperature is greater than 40");
+    
+    
+// }
+// if you check the only one correct conditon and than code is run then you use
+// const temp = 50;
+// if (temp === 50) {
+//     console.log(" the temperatur is greater than 45");
+    
+// } else {
+//     console.log("temperature is less than 45");
+    
+// }ans is the temperatur is greater than 45
+
+// const temp = 40;
+// if (temp === 50) {
+//     console.log(" the temperatur is greater than 45");
+    
+// } else {
+//     console.log("temperature is less than 45");
+    
+// }ans :temperature is less than 45
