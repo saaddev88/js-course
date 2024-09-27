c8bce98cda1aae23cd51a34a8f5e537f20479c74
saaddev88 <m.saad17102002@gmail.com>
2024-09-27 21:27:22 +0000

    one.js file

--- 04_Control_Flow/one.js
+++ 04_Control_Flow/one.js
@@ -37,3 +37,70 @@
 //     console.log("temperature is less than 45");
     
 // }ans :temperature is less than 45
+
+// const score = 200
+
+// if (score >100) {
+//     const power = "fly"
+//     console.log(`user power :${power}`);
+// }
+
+// in if condition which is written in {} this not executed outside of the scope but if we use var then it is executed out of the scope thats why var create confusion in code
+
+// const newuser = 200
+
+// if (newuser > 100) {
+//     var power ="fly"
+//     console.log(`user power :${power}`);
+// }
+// console.log(`user power :${power}`); this create the confusion on your code because it is accessable outside of your scope
+
+// shorthand notation of control statements
+
+// const balance = 500
+
+// if (balance >100) console.log("test:");
+
+// this is how we can add more thing in shorthand notation by using comma(,)
+// const balance = 1000
+
+// if (balance >100) console.log("test:"),console.log(" done");
+// ;
+// nested 
+
+// if (balance < 750) {
+
+//     console.log("balance is less than 750");
+    
+// }
+// else if(balance < 900){
+//     console.log("balance is less than 900");
+    
+// }  
+// else {
+//     console.log("balance is less than 1200");
+    
+    
+// }
+
+const userloggedin = true
+const debitcard = true
+const userloggedinfromgoogle = false
+const userloggedinfromemail = true
+
+
+//in this condition you are checking both things for entering (&& it means and)
+// if (userloggedin && debitcard) {
+//     console.log("Your are enterd");
+    
+// }
+
+// in this case you are checking only one thing which was correct in the condition || (its means or)
+// if (userloggedinfromemail || userloggedinfromgoogle ) {
+//     console.log("yoy are entered from email");
+    
+    
+// }
+
+
+
