73dbfa1427ef14098aca0f81fce7eea49d8bac45
saaddev88 <m.saad17102002@gmail.com>
2024-09-27 22:00:55 +0000

    switch.js file

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,54 @@
+// Basic syntax of switch statement
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("wrong enter");
+        
+//         break;
+// }
+
+// if the value is given in the form of string then this formate is used :
+// const month = "April"
+
+// switch (month) {
+//     case "January":
+//         console.log("January");
+//         break;
+//     case "February":
+//         console.log("February");
+//         break;
+//     case "March":
+//         console.log("March");
+//         break;
+//     case "April":
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("wrong enter");
+        
+//         break;
+// }
