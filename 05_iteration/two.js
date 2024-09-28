dcb4a07873bbdb33269d91669cc851b86bf484a4
saaddev88 <m.saad17102002@gmail.com>
2024-09-28 23:07:56 +0000

    two.js filr

---
+++ 05_iteration/two.js
@@ -0,0 +1,30 @@
+// WHILE AND DO WHILE LOOP
+
+let index = 0
+
+while (index <= 10) {
+    // console.log(`the value of index ${index}`);
+    index = index +2
+}
+
+let myArray = ['ALI','HASAN','HUSSAIN']
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`the value is ${myArray[arr]}`);
+    arr++
+    
+    
+}
+
+
+
+
+let score = 1
+do {
+    // console.log(`the value is ${score}`);
+    score++
+
+
+} while (score <= 10);
+
