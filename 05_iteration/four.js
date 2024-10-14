c06a5ef62f04fdac6aff1b769e859cf32e61f7f1
saaddev88 <m.saad17102002@gmail.com>
2024-10-14 22:43:29 +0000

    fourth file

---
+++ 05_iteration/four.js
@@ -0,0 +1,29 @@
+const myObject ={
+    js : "javascript",
+    cpp : "c++",
+    py : "python"
+}
+for (const key in myObject) {
+    // console.log(`${key} is the shortcut pf the ${myObject[key]} `);
+    
+   
+}
+
+const programming = ["cpp","php","rb","py","js" ]
+for (const key in programming) {
+    
+        // console.log(programming[key]);
+        
+    }
+
+//  map is not iretable for in loop 
+
+//     const map = new Map()
+// map.set('pk','pakistan')
+// map.set('uk','United Kindom')
+// map.set('ksa','Kindom of saudi Arabia')
+// map.set('uk','United Kindom')
+// for (const key in map) {
+//     console.log(map);
+    
+// }
