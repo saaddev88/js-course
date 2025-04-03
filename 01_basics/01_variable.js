47882b15ca969945815fd205a7efad9b6f7837c6
saaddev88 <m.saad17102002@gmail.com>
2025-04-03 22:17:05 +0000

    variables in js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 45536
+let accountEmail = "abc@mail.com"
+var accountPassword ="23444"
+accountCity = "karachi"
+
+// accountId = 33 not allowed to overwrite the const value
+
+console.log(accountId);
+accountEmail ="def@hotmail.com"
+console.log(accountEmail);
+
+accountPassword = "31341441"
+console.log(accountPassword);
+
+accountCity = "lahori"
+console.log(accountCity);
+
+
+
