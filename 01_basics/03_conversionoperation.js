53d6cd8f6d28fe2d27054d55bec8a33a298a274c
saaddev88 <m.saad17102002@gmail.com>
2025-04-04 21:29:17 +0000

    dataconversion in js

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+let score ="saad"
+//awnser is number
+console.log(typeof score);
+
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber); //not a number(nan)
+
+//  "33" => 33
+// "33abc" => nan
+// true => 1,false =>0
+
+let isloggedin = "saad"
+
+let booleanloggedin = Boolean(isloggedin)
+
+console.log( booleanloggedin);
+
+
+// 1=>true,0 =>false
+// ""=>false
+// "hitesh" =>true 
+
+let somenumber = 33
+let stringnumber =String(somenumber)
+console.log(typeof stringnumber);
