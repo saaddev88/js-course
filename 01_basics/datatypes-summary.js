cfa3abc1c856c9b3e07f0a675d416447450bd91d
saaddev88 <m.saad17102002@gmail.com>
2025-04-04 22:27:28 +0000

    datatyes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// primitive datatypes
+// 7 types: string,number,null,undefined,symbol,bigint,boolean
+
+const score = 100
+const scorevalue = 100.3
+
+const isloggedin = false
+const outsidetemp = null
+let useremail;
+const id = Symbol("123")
+const anotherid = Symbol("123")
+
+const bigint = 1256484864568897n
+
+
+
+// non-primitive datatypes(refrencestypes)
+
+// array,objects,functioms
+
+const heros =["batman","spiderman","safeguard"]
+let myobj ={
+
+    name :"saad",
+    age :23,
+    gender:"male"}
+const myfunction = function(){
+
+    console.log("helloworld");
+
+}
+console.log(typeof bigint);
+console.log(typeof myfunction);
+console.log(typeof  useremail);
+console.log(typeof heros);
+
+
+
+
+
