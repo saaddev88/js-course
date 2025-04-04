92b252b9d5e2dc4b768f90d6ae07c2c04499f416
saaddev88 <m.saad17102002@gmail.com>
2025-04-04 21:44:30 +0000

    conversion in js

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -25,3 +25,38 @@ console.log( booleanloggedin);
 let somenumber = 33
 let stringnumber =String(somenumber)
 console.log(typeof stringnumber);
+// ===================================operations================================
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(5-3);
+// console.log(9*5);
+// console.log(45/5);
+// console.log(2**5);
+// console.log(2%3);
+
+// console.log("1"+2+2);
+// console.log(2+"2"+1);
+// console.log(2+2+"2");
+
+// console.log(3+4*5%9); this type of code is not written 
+
+
+let num1,num2,num3= 2+2
+
+let gamecounter = 100
+// ++gamecounter prefixes
+// gamecounter++ postfixes
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
