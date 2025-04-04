db891e7280519e2c41abea693814e3f0811f6be3
saaddev88 <m.saad17102002@gmail.com>
2025-04-04 21:28:53 +0000

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+// your code must readable 
+// easy to understand and maintainanle 
+console.log("saad");
+let name = "saad"
+let age = 16
+let isloggedin =false
+
+// there are multiple data types in js 
+
+// strings =>""
+// int => Number
+// bigint => only use where there was a huge numbers of manupulation
+// boolean => true/false
+// null => standalone value or object
+// undefined => undefined
+// symbols = > unique
+
+// no primitive datatypes
+// Object
+
+console.log(typeof "saad"); //string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
