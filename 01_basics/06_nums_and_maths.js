9b6c810ed815b05b9a35cb80b32afacc3706ee02
saaddev88 <m.saad17102002@gmail.com>
2025-04-06 22:00:20 +0000

    numbers and maths in js

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,12 +1,33 @@
-let score = 400
-console.log(score);
+// let score = 400
+// console.log(score);
 
-let anotherscore = new String(100)
-console.log(anotherscore);
-console.log(anotherscore.toString().length);
-console.log(score.toPrecision(5));
-console.log(score.toFixed(2))
+// let anotherscore = new String(100)
+// console.log(anotherscore);
+// console.log(anotherscore.toString().length);
+// console.log(score.toPrecision(5));
+// console.log(score.toFixed(2))
 
 // *****************maths***************************
+console.log(Math.abs(-19));
+console.log(Math.round(4.3));
+console.log(Math.ceil(5.7));
+console.log(Math.floor(3.9));
+console.log(Math.min(3,6,7,8,9,24,4));
+console.log(Math.max(44,56,77,8,10,11));
+
+console.log((Math.random()*10)+1);
+
+let max = 20
+let min =10
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min);
+
+
+
+
+
+
+
+
 
 
