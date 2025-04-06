37b6eb92c7ef140c9353404f3fcd3fa47a842f09
saaddev88 <m.saad17102002@gmail.com>
2025-04-06 10:01:17 +0000

    numbers and maths in js

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,12 @@
+let score = 400
+console.log(score);
+
+let anotherscore = new String(100)
+console.log(anotherscore);
+console.log(anotherscore.toString().length);
+console.log(score.toPrecision(5));
+console.log(score.toFixed(2))
+
+// *****************maths***************************
+
+
