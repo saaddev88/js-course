d131431dff8cb46baf3c37ed338a14b9f437be01
saaddev88 <m.saad17102002@gmail.com>
2025-04-06 09:59:34 +0000

    strings in js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,30 @@
+let name = "saad"
+let age = 20
+
+
+// old syntax for writing strings
+console.log(name +age);
+
+// modern syntax
+
+console.log(`my name is ${name} and my age is ${age}`);
+
+// another technique for string assigning
+
+const gamneName = new String("saad-pc")
+console.log(typeof gamneName);
+console.log((gamneName.split('-')
+));
+
+console.log((gamneName.toUpperCase())
+);
+console.log(gamneName.substring(0,3));
+console.log(gamneName.slice(-7,-3));
+
+
+
+
+
+
+
+
