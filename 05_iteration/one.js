b4980fdfae372b0e59d1d013525a101748aab8dc
saaddev88 <m.saad17102002@gmail.com>
2025-04-26 23:28:25 +0000

    iteration in js

---
+++ 05_iteration/one.js
@@ -0,0 +1,54 @@
+// for loops
+// for (let i = 0; i < 11; i++) {
+//     const element = i
+//     if (element == 5) {
+//         console.log("5 is the best number");
+        
+        
+//     }
+
+
+//     console.log(i);
+    
+// }
+// for (let i = 0; i < 11; i++) {
+//     console.log(i);
+    
+//     for (let j = 0; j < 11; j++) {
+//         console.log(`innrelopp${j} && outerloop${i}`);
+        
+        
+        
+//     }
+    
+// }
+// for (let i = 0; i < 11; i++) {
+//     console.log(i);
+    
+//     for (let j = 0; j < 11; j++) {
+//         console.log(i + "*" + j +"=" + i*j);
+
+        
+        
+        
+//     }
+    
+// }
+let myArray =['safeguard','jamandar','burqaavenger']
+console.log(myArray.length);
+for (let i = 0; i < myArray.length; i++) {
+  
+    
+
+    
+    const element = myArray[i];
+    // console.log(element.length);
+    
+
+    console.log(element);
+    
+    
+}
+//break and continue statement:
+//break stops the runing code aur series of function and continue resume the sets of function aud other things
+
