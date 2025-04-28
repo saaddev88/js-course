b7fa58469db7377859dcf08940baa1c60eb727a1
saaddev88 <m.saad17102002@gmail.com>
2025-04-28 17:08:13 +0000

    loops in js

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -1,39 +1,39 @@
 // for loops
-// for (let i = 0; i < 11; i++) {
-//     const element = i
-//     if (element == 5) {
-//         console.log("5 is the best number");
+for (let i = 0; i < 11; i++) {
+    const element = i
+    if (element == 5) {
+        console.log("5 is the best number");
         
         
-//     }
+    }
 
 
-//     console.log(i);
+    console.log(i);
     
-// }
-// for (let i = 0; i < 11; i++) {
-//     console.log(i);
+}
+for (let i = 0; i < 11; i++) {
+    console.log(i);
     
-//     for (let j = 0; j < 11; j++) {
-//         console.log(`innrelopp${j} && outerloop${i}`);
+    for (let j = 0; j < 11; j++) {
+        console.log(`innrelopp${j} && outerloop${i}`);
         
         
         
-//     }
+    }
     
-// }
-// for (let i = 0; i < 11; i++) {
-//     console.log(i);
+}
+for (let i = 0; i < 11; i++) {
+    console.log(i);
     
-//     for (let j = 0; j < 11; j++) {
-//         console.log(i + "*" + j +"=" + i*j);
+    for (let j = 0; j < 11; j++) {
+        console.log(i + "*" + j +"=" + i*j);
 
         
         
         
-//     }
+    }
     
-// }
+}
 let myArray =['safeguard','jamandar','burqaavenger']
 console.log(myArray.length);
 for (let i = 0; i < myArray.length; i++) {
@@ -49,6 +49,33 @@ for (let i = 0; i < myArray.length; i++) {
     
     
 }
-//break and continue statement:
-//break stops the runing code aur series of function and continue resume the sets of function aud other things
+// break and continue statement:
+// break stops the runing code aur series of function and continue resume the sets of function aud other things
+
+// for (let index = 1; index < 20; index++) {
+//   if (index == 5) {
+//     console.log("5 is detected!");
+//     break
+    
+    
+//   }
+  
+  
+//   console.log(index);
+  
+  
+// }
+for (let index = 1; index < 20; index++) {
+  if (index == 5) {
+    console.log("5 is detected!");
+    continue
+    
+    
+  }
+  
+  
+  console.log(index);
+  
+  
+}
 
