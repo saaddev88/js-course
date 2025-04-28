4a91565861f438fac70bcedbaba200580860e591
saaddev88 <m.saad17102002@gmail.com>
2025-04-28 17:58:42 +0000

    iteration in js

---
+++ 05_iteration/two.js
@@ -0,0 +1,27 @@
+// while and do while loops:
+
+// let i = 0
+// while (i <= 20) {
+//     console.log(`the value of${i}`);
+//     i+=2
+    
+    
+// }
+const myArray =["superman","ironman","spiderman","batman","captainamerica"]
+
+let  i = 1
+while (i <myArray.length) {
+    console.log(myArray[i]);
+    i++
+    
+    
+}
+// do while loops:
+let testScore = 10
+do {
+    console.log(`best marks${testScore}`);
+    testScore++
+    
+    
+} while (testScore <=90);
+
